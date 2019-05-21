e52250c86460eff63f82f416455785041ec5676a
leslie279 <1395700952@qq.com>
2019-05-21 16:45:39 +0800

    First Commit

---
+++ Student system/Student system/main.m
@@ -0,0 +1,17 @@
+//
+//  main.m
+//  Student system
+//
+//  Created by 陈培旺 on 2019/5/21.
+//  Copyright © 2019 陈培旺. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[]) {
+    @autoreleasepool {
+        // insert code here...
+        NSLog(@"Hello, World!");
+    }
+    return 0;
+}
